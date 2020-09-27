cbe04a55b3ec9964f1f0e02327ae41aad1325032
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-09-28 00:13:51 +0530

    specify the print

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def __init__(self,number,list):
     self.number = number
     self.array = list
 
+print("how long yo want your list is ?")
 number = int(input())
+print("enter numbers...")
 list=[]
 for i in range(number):
     list[i] = int(input())
