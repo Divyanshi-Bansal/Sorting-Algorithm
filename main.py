032224929e1a2885f2b56c664b4f426ef2ca392c
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-09-28 00:12:10 +0530

    rename the array to list

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import InsertionSort
 
-def __init__(self,number,array):
+def __init__(self,number,list):
     self.number = number
-    self.array = array
+    self.array = list
 
 number = int(input())
 list=[]
 for i in range(number):
     list[i] = int(input())
 
+InsertionSort(list)
