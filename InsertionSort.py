550bd5f19b43952affeb0b192662ed82d9beaf32
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-09-28 00:09:21 +0530

    make insertion func.

---
+++ InsertionSort.py
@@ -0,0 +1,5 @@
+import main
+
+def insertion(list):
+    for i in range(len(list)):
+        print(list[i],end="")
---
+++ main.py
@@ -0,0 +1,7 @@
+import InsertionSort
+
+def __init__(self,number,array):
+    self.number = number
+    self.array = array
+
+
