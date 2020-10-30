0da5ea1825e6134bae7d1c596387414fad579efb
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-10-30 14:43:32 +0530

    made InsertionSort class

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,5 +1,12 @@
 import main
 
-def insertion(list):
-    for i in range(0,len(list)):
-        print(list[i])
+class insertionSort:
+    def insertion(list):
+        for i in range(1,len(list)):
+            j=i-1
+            value = list[i]
+            while value<list[j] and j>=0:
+                list[j+1] = list[j]
+                j=j-1
+            list[j] = value
+
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 import InsertionSort
 
 if __name__ == "__main__":
-    print("how long you want your list is ?")
-    number = int(input())
 
-    print("enter numbers...")
-    list=[]
-    for i in range(number):
-        list[i] = input()
-
-    InsertionSort.insertion(list)
