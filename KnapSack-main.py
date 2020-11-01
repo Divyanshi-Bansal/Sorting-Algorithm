c68c0be271052ea985ea3ab99d5668e0de67dc70
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-01 21:12:43 +0530

    complete KnapSack-main.py

---
+++ KnapSack-main.py
@@ -0,0 +1,18 @@
+import knapSack
+
+if __name__ == "__main__":
+    print("how many value want")
+    n= int(input())
+    weight=[]
+    for i in range(n):
+        print("enter weight")
+        weight.append(int(input()))
+    value=[]
+    for i in range(n):
+        print("value of each weight")
+        value.append(int(input()))
+    print("enter capacity ")
+    capacity = int(input())
+
+    maxValue = knapSack.fractionalKnapSack(weight,value,capacity)
+    print("Maximum value of knapSack is "+maxValue)
